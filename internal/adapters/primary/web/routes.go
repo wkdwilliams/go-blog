93ca84d63d7c9d00050755f780f94847ca943c7d
wkdwilliams <31390496+wkdwilliams@users.noreply.github.com>
2024-08-29 18:05:21 +0700

    init

---
+++ internal/adapters/primary/web/routes.go
@@ -0,0 +1,9 @@
+package web
+
+import (
+	"github.com/samverrall/hex-structure/internal/adapters/primary/web/users"
+)
+
+func (a *App) initAppRoutes() {
+	a.fiber.Group("/users", users.CreateAccount(a.userAPI))
+}
---
+++ internal/adapters/primary/web/server.go
@@ -0,0 +1,34 @@
+package web
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/samverrall/hex-structure/internal/core/services/users"
+)
+
+type App struct {
+	fiber   *fiber.App
+	userAPI users.IUserService
+	port    int
+}
+
+func NewApp(userAPI users.IUserService, opts ...AppOption) *App {
+	s := &App{
+		fiber:   fiber.New(),
+		userAPI: userAPI,
+		port:    8000,
+	}
+
+	for _, applyOption := range opts {
+		applyOption(s)
+	}
+
+	s.initAppRoutes()
+
+	return s
+}
+
+func (a *App) Run() error {
+	return a.fiber.Listen(fmt.Sprintf(":%d", a.port))
+}
